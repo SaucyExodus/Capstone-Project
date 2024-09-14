29500c007abd641df45c7906c567a327fb59ae0b
Gage Cates <gage.cates01@gmail.com>
2024-09-14 03:24:06 -0700

    added comments

--- src/listeners/events/app_online.js
+++ src/listeners/events/app_online.js
@@ -1,10 +1,12 @@
-import { appOnlineMessageUI } from '../../user-interface/messages/app_online_message.js'
+import { appOnlineMessageUI } from '../../user-interface/messages/app_online_message.js';
 
+// Initializing a flag for setting the app up
 let setAppPresence = false;
 
 export async function appOnlineEvent(slackEvents, web) {
     if(!setAppPresence) {
         try {
+            // Attempt to post a message to the '#test-bot' channel on Slack
             await web.chat.postMessage({
                 channel: '#test-bot',
                 text: 'Bot is now online!',
@@ -13,6 +15,7 @@ export async function appOnlineEvent(slackEvents, web) {
             console.log('App is online!');
             setAppPresence = true;
         } catch (error) {
+            // Log any errors to the console
             console.error('Error: ', error);
         }        
     }
--- src/user-interface/messages/app_online_message.js
+++ src/user-interface/messages/app_online_message.js
@@ -1,19 +1,19 @@
 export function appOnlineMessageUI() {
     return [
         {
-            "type": "section",
-            "block_id": "section-identifier",
-            "text": {
-                "type": "mrkdwn",
-                "text": "*Welcome to the Slack app!* Click the button to visit the Home tab."
+            type: 'section',
+            block_id: 'section-identifier',
+            text: {
+                type: 'mrkdwn',
+                text: '*Welcome to the Slack app!* Click the button to visit the Home tab.'
             },
-            "accessory": {
-                "type": "button",
-                "text": {
-                    "type": "plain_text",
-                    "text": "Go to App"
+            accessory: {
+                type: 'button',
+                text: {
+                    type: 'plain_text',
+                    text: 'Go to App'
                 },
-                "url": "https://slack.com/app_redirect?channel=D07KGPEBBFC" // Deep link to your Slack channel
+                url: 'https://slack.com/app_redirect?channel=D07KGPEBBFC' // Deep link to your Slack channel
             }
         }
     ];
