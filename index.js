4364b795bf01ed5c9702094b68abbf4ee9f1bfb9
Gage Cates <gage.cates01@gmail.com>
2024-09-13 22:51:39 -0700

    test 2

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import dotenv from 'dotenv';
 import { WebClient } from '@slack/web-api';
 import { createEventAdapter } from '@slack/events-api';
 
-import { registerListeners } from './src/listeners/index';
+import { registerListeners } from './src/listeners';
 
 dotenv.config(); // Load environment variables from .env
 
@@ -21,21 +21,7 @@ app.use(express.json());
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
-  sendMessage('#test-bot', "I'm Online");
 });
 
 registerListeners(slackEvents);
 
-//Function to send a message to a Slack channel
-async function sendMessage(channel, message) {
-  try {
-    const response = await web.chat.postMessage({
-      channel: channel,
-      text: message,
-    });
-    console.log('Welcome message sent: ', response);
-  } catch (error) {
-    // Handle Slack API errors
-    console.error('Error sending message:', error);
-  }
-}
--- src/listeners/events/index.js
+++
@@ -1,7 +0,0 @@
-const { appHomeOpenedEvent } = require('./app_home_opened');
-
-function register(slackEvents) {
-    appHomeOpenedEvent(slackEvents);
-}
-
-module.exports = { register };
