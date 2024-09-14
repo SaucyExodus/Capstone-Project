e4cc9c28e00a268198227c665305e7b1ba164952
Gage Cates <gage.cates01@gmail.com>
2024-09-14 02:34:42 -0700

    auto

--- src/listeners/events/app_online.js
+++ src/listeners/events/app_online.js
@@ -3,7 +3,7 @@ let setAppPresence = false;
 export async function appOnlineEvent(slackEvents, web) {
     if(!setAppPresence) {
         try {
-            await web.users.setPresence({ presence: 'active' });
+            await web.users.setPresence({ presence: 'auto' });
             await web.chat.postMessage({
                 channel: '#test-bot',
                 text: 'Bot is now online!'
