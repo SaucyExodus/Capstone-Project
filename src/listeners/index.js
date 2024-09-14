2f56db0f650068e85f27095455805c881d79f738
Gage Cates <gage.cates01@gmail.com>
2024-09-13 23:58:16 -0700

    POST MESSAGE

--- src/listeners/index.js
+++ src/listeners/index.js
@@ -1,7 +1,11 @@
 export function registerListeners(slackEvents, web) {
     slackEvents.on('app_home_opened', async (event) => {
         try {
-            web.chat.postMessage('#test-bot', "You clicked home tab!")
+            // Ensure you use the correct channel ID or name
+            await web.chat.postMessage({
+                channel: '#test-bot', // Use a valid channel ID or name
+                text: "You clicked the home tab!"
+            });
             console.log("YAYA");
         } catch (error) {
             console.log(error);
