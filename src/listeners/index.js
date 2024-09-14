62046f3d622a6db107f13ae544483a04726773f1
Gage Cates <gage.cates01@gmail.com>
2024-09-14 01:21:02 -0700

    username

--- src/listeners/index.js
+++ src/listeners/index.js
@@ -2,9 +2,8 @@ export function registerListeners(slackEvents, web) {
     slackEvents.on('app_home_opened', async (event) => {
         try {
             // Print Users name in console log when user opens app home
-            const userInfo = await web.users.info({ user: event.user });
-            const userName = userInfo.user.real_name || userInfo.user.name;
-            console.log(`App Home opened by user: `, userName);
+            const userName = await web.users.info({ user: userId });
+            console.log('App Home opened by user: ', userName);
         } catch (error) {
             // Catch errors
             console.log(error);
