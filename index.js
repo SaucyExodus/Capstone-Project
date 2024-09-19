011e93ed87e4f3e41d533552e2a319a6b6e6c0e6
Gage Cates <gtcates@asu.edu>
2024-09-18 18:31:06 -0700

    test send respond

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import express from 'express';
 import { WebClient } from '@slack/web-api';
 import dotenv from 'dotenv';
 
+import { registerListeners } from './src/listeners/index.js';
 dotenv.config(); // Load environment variables from .env
 
 const app = express();
@@ -12,10 +13,10 @@ const web = new WebClient(process.env.SLACK_OAUTH_TOKEN);
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post('/', (req, res) => {
+app.post('/slack/events', (req, res) => {
   const data = req.body;
   console.log(`Received POST data: `, JSON.stringify(data, null, 2));
-  res.send(`Received POST data: ${JSON.stringify(data)}`);
+  //res.send(`Received POST data: ${JSON.stringify(data)}`);
 });
 
 app.listen(port, () => {
