ab5172bbe1822300fbbba4c8c435124512f0aeb2
Gage Cates <gtcates@asu.edu>
2024-09-18 21:38:29 -0700

    console log

--- src/functions/invite_app_channel.js
+++ src/functions/invite_app_channel.js
@@ -2,6 +2,7 @@ export async function inviteAppChannel(web, channelID) {
   try {
     const userList = await web.users.list();
     const users = userList.members;
+    console.log(channelID);
 
     for (const user of users) {
       if (!user.is_bot && !user.deleted) {
