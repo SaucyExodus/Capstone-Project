05e4b79d4b56b0aff67bea419cf2a3c7e96e36c6
Gage Cates <gtcates@asu.edu>
2024-09-18 17:57:52 -0700

    again

--- src/listeners/index.js
+++ src/listeners/index.js
@@ -1,5 +1,5 @@
 import { eventListener } from './events/index.js';
-import { shortcutListner } from './shortcuts/index.js';
+import { shortcutListener } from './shortcuts/index.js';
 
 export function registerListeners(slackActivity, web) {
     eventListener(slackActivity, web);
