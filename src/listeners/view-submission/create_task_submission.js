61aa148426d42d01aec43738e495e3515a35f873
Gage Cates <gtcates@asu.edu>
2024-09-19 01:36:06 -0700

    extracting task data.

---
+++ src/listeners/view-submission/create_task_submission.js
@@ -0,0 +1,20 @@
+export async function createTaskSubmission(slackActivity, web) {
+  try {
+    const { view, user } = slackActivity;
+    const taskData = {
+      userId: user.id,
+      taskName: view.state.values["task_name_input"]["task_name_action"].value,
+      assignedUsers: view.state.values["assign_user_input"]["assign_user_action"].selected_users,
+      dueDate: view.state.values["due_date_input"]["due_date_action"].selected_date,
+      dueTime: view.state.values["time_input"]["time_action"].selected_time,
+    };
+
+    console.log("Extracted Task Data:", taskData);
+
+    // Save task data (implement the `saveTaskData` function in your database module)
+
+
+  } catch (error) {
+    console.error("Error handling view submission:", error);
+  }
+}
--- src/listeners/view-submission/index.js
+++ src/listeners/view-submission/index.js
@@ -1,10 +1,12 @@
+import { createTaskSubmission } from './create_task_submission.js'; 
+
 export function viewSubmissionListener(slackActivity, web) {
   // Correctly access the callback_id from slackActivity.view
   const callbackId = slackActivity.view.callback_id;
 
   switch (callbackId) {
     case "create_task_modal":
-      console.log("Yay you submitted it");
+      createTaskSubmission(slackActivity, web);
       break;
 
     default:
