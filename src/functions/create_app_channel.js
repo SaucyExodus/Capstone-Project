e399bb926cfc107d614f2219754792f8748c0297
Gage Cates <gtcates@asu.edu>
2024-09-18 21:30:30 -0700

    check if channel exist already

--- src/functions/create_app_channel.js
+++ src/functions/create_app_channel.js
@@ -2,10 +2,30 @@ export async function createAppChannel(web) {
   const channelName = "task-simply";
 
   try {
-    await web.conversations.create({
-      name: channelName,
-      is_private: false,
+    // Check if the channel already exists
+    const channelListResponse = await web.conversations.list({
+      exclude_archived: true,
+      types: "public_channel,private_channel",
     });
+
+    const existingChannel = channelListResponse.channels.find(
+      (channel) => channel.name === channelName
+    );
+
+    if (existingChannel) {
+      console.log(
+        `Channel ${channelName} already exists with ID: ${existingChannel.id}`
+      );
+      return existingChannel.id;
+    } else {
+      const createChannelResponse = await web.conversations.create({
+        name: channelName,
+        is_private: false,
+      });
+      const channelId = createChannelResponse.channel.id;
+      console.log(`Channel ${channelName} created with ID: ${channelId}`);
+      return channelId;
+    }
   } catch (error) {
     console.error(error);
   }
