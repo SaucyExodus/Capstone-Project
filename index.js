c9d76f95059a3aa285d84e8915c2e82e2ce4d5a9
Gage Cates <gtcates@asu.edu>
2024-09-18 18:53:13 -0700

    slack interactions

--- index.js
+++ index.js
@@ -24,6 +24,13 @@ app.post("/slack/events", (req, res) => {
   }
 });
 
+app.post("/slack/interactions", (req, res) => {
+  const payload = req.body.payload;
+
+  console.log(`Received POST data: `, JSON.stringify(payload, null, 2));
+  res.sendStatus(200);
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
