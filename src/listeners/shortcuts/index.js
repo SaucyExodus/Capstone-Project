f4bb0b4ea3e4b98c52518385e6eb98dceed18b8e
Gage Cates <gtcates@asu.edu>
2024-09-19 03:33:26 -0700

    daw

--- src/listeners/shortcuts/index.js
+++ src/listeners/shortcuts/index.js
@@ -5,6 +5,10 @@ export function shortcutListener(slackActivity, web) {
         case 'global_create_task':
             globalCreateTask(slackActivity, web);
             break;
+
+        case 'test_callback':
+            console.log("please");
+
         default:
             console.log(`Couldn't find callback id.`);
             break;
