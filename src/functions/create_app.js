94331b1bebec5dd18f2fe74364f58ee1f9f3d9c7
Gage Cates <gtcates@asu.edu>
2024-09-18 22:09:19 -0700

    please

--- src/functions/create_app.js
+++ src/functions/create_app.js
@@ -43,7 +43,7 @@ async function inviteAppChannel(web, channelID) {
   
       for (const user of users) {
         console.log(user);
-        if (!user.is_bot && !user.deleted && !userList.has(user.id)) {
+        if (!user.is_bot && !user.deleted && !(user.id == 'USLACKBOT')) {
           await web.conversations.invite({
             channel: channelID,
             users: user.id,
