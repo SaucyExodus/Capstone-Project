6257566042d94b08060ffe7b7b0d9838bc0a52d1
Gage Cates <gtcates@asu.edu>
2024-09-19 03:44:01 -0700

    test

--- src/listeners/shortcuts/index.js
+++ src/listeners/shortcuts/index.js
@@ -1,13 +1,39 @@
 import { globalCreateTask } from './global_create_task.js';
 
-export function shortcutListener(slackActivity, web) {
+export async function shortcutListener(slackActivity, web) {
     switch (slackActivity.callback_id) {
         case 'global_create_task':
             globalCreateTask(slackActivity, web);
             break;
 
         case 'test_callback':
-            console.log(slackActivity);
+            await web.views.open({
+                "trigger_id": slackActivity.trigger_id,
+                    "type": "modal",
+                    "callback_id": "modal-identifier",
+                    "title": {
+                      "type": "plain_text",
+                      "text": "Just a modal"
+                    },
+                    "blocks": [
+                      {
+                        "type": "section",
+                        "block_id": "section-identifier",
+                        "text": {
+                          "type": "mrkdwn",
+                          "text": "*Welcome* to ~my~ Block Kit _modal_!"
+                        },
+                        "accessory": {
+                          "type": "button",
+                          "text": {
+                            "type": "plain_text",
+                            "text": "Just a button",
+                          },
+                          "action_id": "button-identifier",
+                        }
+                      }
+                    ],  
+            });
             break;
 
         default:
