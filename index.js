5b4892b96c6c0eae0e697e800b2247849b89cc9a
Gage Cates <gtcates@asu.edu>
2024-09-18 18:09:15 -0700

    testing json data

--- index.js
+++ index.js
@@ -1,36 +1,22 @@
 import express from 'express';
-import dotenv from 'dotenv';
-import bodyParser from 'body-parser';
 import { WebClient } from '@slack/web-api';
-import { createEventAdapter } from '@slack/events-api';
-
-import { registerListeners } from './src/listeners/index.js';
+import dotenv from 'dotenv';
 
 dotenv.config(); // Load environment variables from .env
 
 const app = express();
 const port = 80;
 
-// Slack OAuth tokens asnd Signing Secret
 const web = new WebClient(process.env.SLACK_OAUTH_TOKEN);
-const slackEvents = createEventAdapter(process.env.SLACK_SIGNING_SECRET)
 
-// Body parser
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
 
-app.post('/slack/interactions', (req, res) => {
-  const data = JSON.parse(req.body.payload);
-  registerListeners(data, web);
-  console.log(`Received POST data: `, JSON.stringify(data, null, 2));
+app.post('/', (req, res) => {
+  const data = req.body;
+  console.log(`Received POST data: ${JSON.stringify(data)}`);
   res.send(`Received POST data: ${JSON.stringify(data)}`);
 });
 
-// Starts Server
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-// Use the slackEvents middleware to handle Slack events
-app.use('/slack/events', slackEvents.expressMiddleware());
-registerListeners(slackEvents, web);
-
