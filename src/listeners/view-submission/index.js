d592dceaf39bb52dd4cb50b6754b2259502a20e3
Gage Cates <gtcates@asu.edu>
2024-09-19 03:24:53 -0700

    await

--- src/listeners/view-submission/index.js
+++ src/listeners/view-submission/index.js
@@ -1,12 +1,12 @@
 import { createTaskSubmission } from './create_task_submission.js'; 
 
-export function viewSubmissionListener(slackActivity, web) {
+export async function viewSubmissionListener(slackActivity, web) {
   // Correctly access the callback_id from slackActivity.view
   const callbackId = slackActivity.view.callback_id;
 
   switch (callbackId) {
     case "create_task_modal":
-      createTaskSubmission(slackActivity, web);
+      await createTaskSubmission(slackActivity, web);
       break;
 
     default:
