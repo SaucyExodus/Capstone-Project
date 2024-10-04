d30aa32a03b588e12cb17511dd749f9c84f3bac7
Gage Cates <gtcates@asu.edu>
2024-10-03 18:52:44 -0700

    Re formated

--- src/listeners/view-submission/create_task_submission.js
+++ src/listeners/view-submission/create_task_submission.js
@@ -1,4 +1,3 @@
-import dayjs from 'dayjs'; // Import dayjs for date formatting
 import { createdTaskMessage } from '../../user-interface/messages/created_task_message.js';
 import { TODO, IN_PROGRESS, DONE } from '../../constants/taskStatus.js';
 
@@ -8,22 +7,14 @@ export async function createTaskSubmission(slackActivity, web) {
     const taskData = {
       userId: user.id,
       taskName: view.state.values["task_name_input"]["task_name_action"].value,
-      dueDateTime: view.state.values["due_datetime_input"]["due_datetime_action"].selected_date_time,
       assignedUsers: view.state.values["assign_user_input"]["assign_user_action"].selected_users,
+      dueDateTime: view.state.values["due_datetime_input"]["due_datetime_action"].selected_date_time,
       taskNotes: view.state.values["notes_input"]["notes_action"].rich_text_value,
       taskStatus: TODO,
     };
 
     console.log("Extracted Task Data:", taskData);
     
-    /*
-    const unixTimestamp = taskData.dueDateTime;
-    const date = new Date(unixTimestamp * 1000);
-
-    console.log(date.toString());
-
-    const formattedDueDateTime = dayjs.unix(taskData.dueDateTime).format('MMMM D, YYYY h:mm A');
-    */
 
     // Send the message to Slack
     await web.chat.postMessage(createdTaskMessage(taskData));
