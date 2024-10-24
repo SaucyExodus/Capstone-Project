98be32eaa362c445f3899563cef4c79a751b3662
Gage Cates <gtcates@asu.edu>
2024-10-23 22:17:53 -0700

    omg

--- src/functions/getTaskData.js
+++ src/functions/getTaskData.js
@@ -1,4 +1,15 @@
+import connection from '../config/db.js';
 
 export async function getTaskData(taskId) {
-    
+  return new Promise((resolve, reject) => {
+    connection.query('SELECT * FROM tasks WHERE id = ?', [taskId], (error, results) => {
+      if (error) {
+        return reject(error);
+      }
+      if (results.length === 0) {
+        return reject(new Error('Task not found'));
+      }
+      resolve(results[0]);
+    });
+  });
 }
