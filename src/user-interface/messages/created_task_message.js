c5b15edf666cf19b9b7a13a5be6b0f223b06876a
Gage Cates <gtcates@asu.edu>
2024-10-23 20:12:17 -0700

    bug fix #3

--- src/user-interface/messages/created_task_message.js
+++ src/user-interface/messages/created_task_message.js
@@ -19,7 +19,7 @@ export function createdTaskMessage(taskData) {
         accessory: {
           type: "button",
           action_id: "view_task_button",
-          value: taskData.taskID.toString(),
+          value: taskData.taskId.toString(),
           text: {
             type: "plain_text",
             text: "View Task",
