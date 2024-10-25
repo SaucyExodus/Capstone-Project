2c7e1f487bc7f55bfe84650d7eb3e5057622ebd3
Gage Cates <gtcates@asu.edu>
2024-10-25 00:39:25 -0700

    this is stupid

--- src/user-interface/modules/task_view_ui.js
+++ src/user-interface/modules/task_view_ui.js
@@ -1,5 +1,6 @@
 export function createViewTaskModal(taskData, web) {
   const taskNameText = `${taskData.task_name}`;
+  const taskAuthorText = "";
   const dueDateText = taskData.due_date ? `Due date: *${taskData.due_date}*` : "No Due Date";
   const assignedUsersText = JSON.parse(taskData.assigned_users).map((user) => `<@${user}>`).join("\n");
   const taskNotesText = taskData.task_notes ? JSON.parse(taskData.task_notes) : { type: "section", text: {type: "mrkdwn", text: "No notes", }, };
@@ -7,7 +8,7 @@ export function createViewTaskModal(taskData, web) {
   (async () => {
     try {
         const userInfo = await web.users.info({ user: taskData.created_by });
-        const taskAuthorText = `Author ${userInfo.user.real_name}`;
+        taskAuthorText = `Author ${userInfo.user.real_name}`;
         console.log(taskAuthorText); // Add this line to see the result
     } catch (error) {
         console.error("Error fetching user info:", error);
