5afdfe5b5d90e6acc2c1503c54593da9c7df8a62
Gage Cates <gtcates@asu.edu>
2024-10-25 00:40:52 -0700

    please

--- src/user-interface/modules/task_view_ui.js
+++ src/user-interface/modules/task_view_ui.js
@@ -1,19 +1,13 @@
 export function createViewTaskModal(taskData, web) {
   const taskNameText = `${taskData.task_name}`;
-  const taskAuthorText = "";
+  const userInfo = web.users.info({ user: taskData.created_by });
+  const taskAuthorText = `Author ${userInfo.user.real_name}`;
   const dueDateText = taskData.due_date ? `Due date: *${taskData.due_date}*` : "No Due Date";
   const assignedUsersText = JSON.parse(taskData.assigned_users).map((user) => `<@${user}>`).join("\n");
   const taskNotesText = taskData.task_notes ? JSON.parse(taskData.task_notes) : { type: "section", text: {type: "mrkdwn", text: "No notes", }, };
 
-  (async () => {
-    try {
-        const userInfo = await web.users.info({ user: taskData.created_by });
-        taskAuthorText = `Author ${userInfo.user.real_name}`;
+
         console.log(taskAuthorText); // Add this line to see the result
-    } catch (error) {
-        console.error("Error fetching user info:", error);
-    }
-  })();
 
 
   const modal = {
