6557d027295bb67da778c289c04fa78bf63df978
Gage Cates <gtcates@asu.edu>
2024-10-25 00:13:00 -0700

    rollback 2

--- src/user-interface/modules/task_view_ui.js
+++ src/user-interface/modules/task_view_ui.js
@@ -1,6 +1,6 @@
 export function createViewTaskModal(taskData) {
   const taskNameText = `${taskData.task_name}`;
-  const taskAuthorText = `Author <@${taskData.created_by}>`;
+  const taskAuthorText = `Author @${taskData.created_by}`;
   const dueDateText = taskData.due_date ? `Due date: *${taskData.due_date}*` : "No Due Date";
   const assignedUsersText = JSON.parse(taskData.assigned_users).map((user) => `<@${user}>`).join("\n");
   const taskNotesText = taskData.task_notes ? JSON.parse(taskData.task_notes) : { type: "section", text: {type: "mrkdwn", text: "No notes", }, };
@@ -33,7 +33,7 @@ export function createViewTaskModal(taskData) {
       },
       {
         type: "context",
-        text: [
+        elements: [
           {
             type: "plain_text",
             text: taskAuthorText,
