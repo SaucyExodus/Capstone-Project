2fbe4a25a2701d8416a4cb2418542b9878cffdde
Hunter Smith <hjsmit14@asu.edu>
2024-10-24 17:03:07 -0700

    a

--- src/listeners/events/index.js
+++ src/listeners/events/index.js
@@ -3,6 +3,19 @@ import { teamJoinedEvent } from "./team_join.js";
 import { appOnlineEvent } from "./app_online.js";
 import { createTaskSubmission } from "../view-submission/create_task_submission.js"; // Adjust the path as needed
 
+export function setupEventListeners(app) {
+  // Listen for the app_home_opened event
+  app.event('app_home_opened', async ({ event, client, context }) => {
+    await appHomeOpenedEvent({ event, client, context });
+  });
+
+  // Listen for the create_task_modal view submission
+  app.view('create_task_modal', async ({ ack, body, view, client, context }) => {
+    await ack();
+    await createTaskSubmission({ view, user: body.user }, client);
+  });
+}
+
 export function eventListener(slackActivity, web) {
   switch (slackActivity.event.type) {
     case "app_home_opened":
@@ -18,13 +31,3 @@ export function eventListener(slackActivity, web) {
       break;
   }
 }
-
-// Assuming you are using Bolt for JavaScript
-app.event('app_home_opened', async ({ event, client, context }) => {
-  await appHomeOpenedEvent({ event, client, context });
-});
-
-app.view('create_task_modal', async ({ ack, body, view, client, context }) => {
-  await ack();
-  await createTaskSubmission({ view, user: body.user }, client);
-});
