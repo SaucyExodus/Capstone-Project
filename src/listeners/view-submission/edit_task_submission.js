0736e91629e927c279101d4021b9edfa76d3900f
Gage Cates <gage.cates01@gmail.com>
2024-10-30 20:57:59 -0700

    update task hope

---
+++ src/listeners/view-submission/edit_task_submission.js
@@ -0,0 +1,18 @@
+export async function editTaskSubmission(slackActivity, web) {
+    try {
+        const { view } = slackActivity;
+        const updatedTaskData = {
+          taskId: view.private_metadata,
+          taskName: view.state.values["task_name_input"]["task_name_action"].value,
+          assignedUsers: view.state.values["assign_user_input"]["assign_user_action"].selected_users,
+          dueDateTime: view.state.values["due_datetime_input"]["due_datetime_action"].selected_date_time,
+          taskNotes: view.state.values["notes_input"]["notes_action"].rich_text_value,
+          taskStatus: view.state.values["status_input"]["status_action"].selected_option.value,
+        };
+
+        await updatedTaskData(taskData);
+        console.log("Extracted Task Data:", taskData);
+    } catch (error) {
+
+    }
+}
