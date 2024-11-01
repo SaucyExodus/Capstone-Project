40108b4e139efe22959b1809be856c0571b46c64
Hunter Smith <hjsmit14@asu.edu>
2024-11-01 12:50:24 -0700

    a

--- src/user-interface/app-home/home_tab.js
+++ src/user-interface/app-home/home_tab.js
@@ -181,6 +181,8 @@ export async function appHomeOpenedUI(userId, justCompletedTaskId = null) {
   // Get the oldest 5 TODO tasks
   toDoTasks = toDoTasks.slice(0, 5).flatMap(({ taskBlock }) => [taskBlock, { type: "divider" }]);
 
+  completedTasks.reverse();
+
   // Get the 5 most recent completed tasks
   completedTasks = completedTasks.slice(0, 10); // 5 tasks + 5 dividers
 
