6b00b9a02b96f5dd2856b0b2a3ed3436fcde3dfc
Hunter Smith <hjsmit14@asu.edu>
2024-11-04 18:37:04 -0700

    Jesus I forget everything

--- src/functions/removeTaskData.js
+++ src/functions/removeTaskData.js
@@ -2,7 +2,7 @@ import db from "../config/db.js";
 
 export async function removeTaskData(taskId) {
   try {
-    const query = "DELETE FROM tasks WHERE id = ?";
+    const query = "DELETE FROM task_card WHERE id = ?";
     db.query(query, [taskId], (error, results) => {
       if (error) {
         console.error(`Error deleting task with ID ${taskId}:`, error);
