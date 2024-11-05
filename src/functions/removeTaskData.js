4e66cf6dd8262988835844465f7413d573eaec3f
Hunter Smith <hjsmit14@asu.edu>
2024-11-04 18:35:32 -0700

    Now this should really fix the delete button

--- src/functions/removeTaskData.js
+++ src/functions/removeTaskData.js
@@ -3,8 +3,13 @@ import db from "../config/db.js";
 export async function removeTaskData(taskId) {
   try {
     const query = "DELETE FROM tasks WHERE id = ?";
-    await db.execute(query, [taskId]);
-    console.log(`Task with ID ${taskId} has been deleted.`);
+    db.query(query, [taskId], (error, results) => {
+      if (error) {
+        console.error(`Error deleting task with ID ${taskId}:`, error);
+        return;
+      }
+      console.log(`Task with ID ${taskId} has been deleted.`);
+    });
   } catch (error) {
     console.error(`Error deleting task with ID ${taskId}:`, error);
   }
