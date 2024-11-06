1b653841e60b07d4506265ec7f0c300a2b481f5f
Hunter Smith <hjsmit14@asu.edu>
2024-11-05 18:45:20 -0700

    spacing

--- src/user-interface/app-home/home_tab.js
+++ src/user-interface/app-home/home_tab.js
@@ -202,8 +202,8 @@ export async function appHomeOpenedUI(userId) {
   completedTasks = completedTasks.slice(0, 5).flatMap(({ taskBlock }) => [taskBlock, { type: "divider" }]);
 
   // Insert tasks into their respective sections
-  blocks.splice(inProgressIndex + 2, 0, ...inProgressTasks);
-  blocks.splice(toDoIndex +  + inProgressTasks.length, 0, ...toDoTasks);
+  blocks.splice(inProgressIndex + 3, 0, ...inProgressTasks);
+  blocks.splice(toDoIndex + 3 + inProgressTasks.length, 0, ...toDoTasks);
   blocks.splice(12 + inProgressTasks.length + toDoTasks.length, 0, ...completedTasks);
 
   return {
