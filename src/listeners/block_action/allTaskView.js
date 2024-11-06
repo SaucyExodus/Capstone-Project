c51b4646d9274b6f46014da582216314d27e6867
Gage Cates <gtcates@asu.edu>
2024-11-05 20:56:45 -0700

    view id console

--- src/listeners/block_action/allTaskView.js
+++ src/listeners/block_action/allTaskView.js
@@ -4,6 +4,7 @@ import { getSavedTasks } from '../../functions/getSavedTasks.js';
 export async function all_task_view(slackActivity, web, taskStatus) {
   try {
     const tasks = await getSavedTasks(slackActivity.user.id); // Fetch saved tasks assigned to the user
+    console.log("Viewing ID: ", slackActivity.constainer.view_id);
     await web.views.open({
       trigger_id: slackActivity.trigger_id,
       private_metadata: slackActivity.view.id,
