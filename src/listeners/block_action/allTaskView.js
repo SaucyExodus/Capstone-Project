9f41effcb1f5ca1533fb9d9446c738e39e5c5c83
Gage Cates <gtcates@asu.edu>
2024-11-18 22:35:32 -0700

    test open view all task

--- src/listeners/block_action/allTaskView.js
+++ src/listeners/block_action/allTaskView.js
@@ -4,10 +4,19 @@ import { getSavedTasks } from '../../functions/getSavedTasks.js';
 export async function all_task_view(slackActivity, web, taskStatus, pageNumber) {
   try {
     const tasks = await getSavedTasks(slackActivity.user.id); // Fetch saved tasks assigned to the user
-    const result = await web.views.open({
-      trigger_id: slackActivity.trigger_id,
-      view: viewAllTasks(taskStatus, tasks, pageNumber)
-    });
+    let result;
+
+    if (viewId) {
+      result = await web.views.update({
+        view_id: viewId,
+        view: viewAllTasks(taskStatus, tasks, pageNumber)
+      });
+    } else {
+      result = await web.views.open({
+        trigger_id: slackActivity.trigger_id,
+        view: viewAllTasks(taskStatus, tasks, pageNumber)
+      });
+    }
     return result.view.id;
   } catch (error) {
     console.error("Error opening view:", error);
