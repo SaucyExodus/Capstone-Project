2c22aed182061af50aabed9c59b7e5e24246dd27
Gage Cates <gtcates@asu.edu>
2024-11-18 23:40:36 -0700

    Some what there i need sleep

--- src/user-interface/modules/view_all_tasks_ui.js
+++ src/user-interface/modules/view_all_tasks_ui.js
@@ -8,8 +8,6 @@ export function viewAllTasks(taskStatus, tasks, pageNumber) {
     const filteredTasks = tasks.filter(task => task.task_status === taskStatus);
     const taskCount = filteredTasks.length;
     const totalPages = Math.ceil(taskCount / 5);
-
-    console.log("Page Number: ", pageNumber);
   
     const taskBlocks = paginateObjects(filteredTasks, pageNumber).map(task => ({
       type: "section",
@@ -98,31 +96,3 @@ export function viewAllTasks(taskStatus, tasks, pageNumber) {
     // Return the sliced list
     return pageItems;
   }
-  
-  // Example usage:
-  
-  // List of objects (can be any type of objects you want)
-  // const objectsList = [
-  //   { id: 1, name: 'Object 1' },
-  //   { id: 2, name: 'Object 2' },
-  //   { id: 3, name: 'Object 3' },
-  //   { id: 4, name: 'Object 4' },
-  //   { id: 5, name: 'Object 5' },
-  //   { id: 6, name: 'Object 6' },
-  //   { id: 7, name: 'Object 7' },
-  //   { id: 8, name: 'Object 8' },
-  //   { id: 9, name: 'Object 9' },
-  //   { id: 10, name: 'Object 10' },
-  //   { id: 11, name: 'Object 11' },
-  //   { id: 12, name: 'Object 12' }
-  // ];
-  
-  // // Get the first page (5 objects)
-  // //console.log(paginateObjects(objectsList, 1));
-  
-  // // Get the second page (next 5 objects)
-  // //console.log(paginateObjects(objectsList, 2));
-  
-  // // Get the third page (last 2 objects)
-  // //console.log(paginateObjects(objectsList, 3));
-  
