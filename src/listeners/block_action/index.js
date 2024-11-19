7888ec266e4a85476ed2bb323b8af2a08b523c3f
Gage Cates <gtcates@asu.edu>
2024-11-18 23:00:54 -0700

    Test

--- src/listeners/block_action/index.js
+++ src/listeners/block_action/index.js
@@ -6,6 +6,7 @@ import { all_task_view } from "./allTaskView.js";
 
 const viewIdStore = {};
 const pageNumberStore = {};
+const statusStore = {};
 
 export async function blockActionListener(slackActivity, web) {
   const actionId = slackActivity.actions[0]?.action_id;
@@ -13,6 +14,8 @@ export async function blockActionListener(slackActivity, web) {
 
   let viewId = viewIdStore[userId];
   let pageNumber = pageNumberStore[userId] || 1;
+  let status = statusStore[userId];
+  
 
   switch (actionId) {
     case "new_task":
@@ -22,6 +25,8 @@ export async function blockActionListener(slackActivity, web) {
     case "view_task_button":
       viewTaskAction(slackActivity, web, viewIdStore[userId]);
       delete viewIdStore[userId];
+      delete pageNumberStore[userId];
+      delete statusStore[userId];
       break;
 
     case "help_modal":
@@ -33,31 +38,40 @@ export async function blockActionListener(slackActivity, web) {
       break;
 
     case "view_more_in_progress":
-      viewId = await all_task_view(slackActivity, web, 'IN_PROGRESS', pageNumber);
+      pageNumberStore[userId] = 1;
+      status = 'IN_PROGRESS';
+      statusStore[userId] = status;
+      viewId = await all_task_view(slackActivity, web, statusStore[userId], pageNumberStore[userId]);
       viewIdStore[userId] = viewId;
       break;
 
     case "view_more_todo":
-      viewId = await all_task_view(slackActivity, web, 'TODO', pageNumber);
+      pageNumberStore[userId] = 1;
+      status = 'TODO';
+      statusStore[userId] = status;
+      viewId = await all_task_view(slackActivity, web, statusStore[userId], pageNumberStore[userId]);
       viewIdStore[userId] = viewId;
       break;
 
     case "view_more_completed":
-      viewId = await all_task_view(slackActivity, web, 'DONE', pageNumber);
+      pageNumberStore[userId] = 1;
+      status = 'DONE';
+      statusStore[userId] = status;
+      viewId = await all_task_view(slackActivity, web, statusStore[userId], pageNumberStore[userId]);
       viewIdStore[userId] = viewId;
       break;
 
     case "next":
       pageNumber += 1;
       pageNumberStore[userId] = pageNumber;
-      viewId = await all_task_view(slackActivity, web, 'TODO', pageNumber, viewId); // You might want to change 'IN_PROGRESS' to the appropriate task status
+      viewId = await all_task_view(slackActivity, web, statusStore[userId], pageNumberStore[userId], viewIdStore[userId]); // You might want to change 'IN_PROGRESS' to the appropriate task status
       viewIdStore[userId] = viewId;
       break;
 
     case "previous":
       pageNumber = Math.max(1, pageNumber - 1); // Ensure the page number does not go below 1
       pageNumberStore[userId] = pageNumber;
-      viewId = await all_task_view(slackActivity, web, 'TODO', pageNumber, viewId); // You might want to change 'IN_PROGRESS' to the appropriate task status
+      viewId = await all_task_view(slackActivity, web, statusStore[userId], pageNumberStore[userId], viewIdStore[userId]); // You might want to change 'IN_PROGRESS' to the appropriate task status
       viewIdStore[userId] = viewId;
       break;
 
