a4794cc7a6e625eaf8e0bbe89c92f960a192230f
Hunter Smith <hjsmit14@asu.edu>
2024-11-19 17:13:08 -0700

    as

--- src/user-interface/modules/help_modal_ui.js
+++ src/user-interface/modules/help_modal_ui.js
@@ -4,12 +4,12 @@ export function helpModal() {
       title: {
         type: "plain_text",
         text: "Help",
-        emoji: true,
+        emoji: true
       },
       close: {
         type: "plain_text",
         text: "Close",
-        emoji: true,
+        emoji: true
       },
       blocks: [
         {
@@ -17,77 +17,77 @@ export function helpModal() {
           text: {
             type: "plain_text",
             text: "Creating a new task",
-            emoji: true,
-          },
+            emoji: true
+          }
         },
         {
           type: "section",
           text: {
             type: "plain_text",
             text: "To create a new task, locate the 'Create New Task' button located at the top of the home page. Then, fill out the information in the pop-up.",
-            emoji: true,
-          },
+            emoji: true
+          }
         },
         {
           type: "image",
           image_url:
             "https://drive.google.com/uc?export=view&id=1MaBYCFyUSIrM7C3zTMjw1sETU-pqoBhe",
-          alt_text: "Screenshot of Create New Task button",
+          alt_text: "Screenshot of Create New Task button"
         },
         {
           type: "header",
           text: {
             type: "plain_text",
             text: "Moving a task to a different progress state",
-            emoji: true,
-          },
+            emoji: true
+          }
         },
         {
           type: "section",
           text: {
             type: "plain_text",
             text: "To change the progress state of a task, navigate to the home page and click the 'View Task' button on the right hand side of the desired task. ",
-            emoji: true,
-          },
+            emoji: true
+          }
         },
         {
           type: "image",
           image_url:
             "https://drive.google.com/uc?export=view&id=19WXlpQid6jox8h5WkEiwIpEh_lmIIqcq",
-          alt_text: "Screenshot of View Task button",
+          alt_text: "Screenshot of View Task button"
         },
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: "Within the pop-up window, select the 'Edit Task' button on the bottom right hand side.",
-          },
+            text: "Within the pop-up window, select the 'Edit Task' button on the bottom right hand side."
+          }
         },
         {
           type: "image",
           image_url:
             "https://drive.google.com/uc?export=view&id=1Qhf0eop2FuF3NqSgSM4N3c24A0QBTYBc",
-          alt_text: "Screenshot of Edit Task button",
+          alt_text: "Screenshot of Edit Task button"
         },
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: "Click the dropdown in the status section and choose a new status for the task. ",
-          },
+            text: "Click the dropdown in the status section and choose a new status for the task. "
+          }
         },
         {
           type: "image",
           image_url:
             "https://drive.google.com/uc?export=view&id=1Uvau8huTm4DfzEK2qvaBxCtdQ9IFTy__",
-          alt_text: "Screenshot of Status dropdown",
+          alt_text: "Screenshot of Status dropdown"
         },
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: "Click 'Done' and these changes will be reflected immediately. ",
-          },
+            text: "Click 'Done' and these changes will be reflected immediately. "
+          }
         },
         {
           type: "image",
@@ -100,16 +100,16 @@ export function helpModal() {
           text: {
             type: "plain_text",
             text: "Editing a task",
-            emoji: true,
-          },
+            emoji: true
+          }
         },
         {
           type: "section",
           text: {
             type: "plain_text",
             text: "To edit a task, navigate to the home page and click the 'View Task' button on the right hand side of the desired task. ",
-            emoji: true,
-          },
+            emoji: true
+          }
         },
         {
           type: "image",
@@ -121,34 +121,34 @@ export function helpModal() {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: "Within the pop-up window, select the 'Edit Task' button on the bottom right hand side.",
-          },
+            text: "Within the pop-up window, select the 'Edit Task' button on the bottom right hand side."
+          }
         },
         {
           type: "image",
           image_url:
             "https://drive.google.com/uc?export=view&id=1Qhf0eop2FuF3NqSgSM4N3c24A0QBTYBc",
-          alt_text: "Screenshot of Edit Task button",
+          alt_text: "Screenshot of Edit Task button"
         },
         {
           type: "section",
           text: {
             type: "mrkdwn",
-            text: "You will then be able to update any details of the task (as marked by the arrows).",
-          },
+            text: "You will then be able to update any details of the task (as marked by the arrows)."
+          }
         },
         {
           type: "image",
           image_url:
             "https://drive.google.com/uc?export=view&id=1Vgdn5hCbzDxbT7w4pUZduI79LH9ptHC3",
-          alt_text: "edit task view",
+          alt_text: "edit task view"
         },
         {
           type: "section",
           text: {
             type: "mrkdwn",
             text: "Once you're finished, click 'Done' to save your changes, or 'Cancel' to discard any modifications.",
-          },
+          }
         },
         {
           type: "image",
@@ -161,19 +161,19 @@ export function helpModal() {
           text: {
             type: "plain_text",
             text: "Deleting a task",
-            emoji: true,
-          },
+            emoji: true
+          }
         },
         {
           type: "section",
           text: {
             type: "plain_text",
             text: "To delete a task, start by navigating to the home page and clicking the 'Expand' button located on the right side of the task you wish to modify. In the pop-up window, select the Edit Task' button.  You should now see a ‘DELETE TASK’ button which will delete the given task.",
-            emoji: true,
-          },
-        },
-      ],
-    };
+            emoji: true
+          }
+        }
+      ]
+    }
   
     return modal;
   }
