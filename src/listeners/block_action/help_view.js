c2318baed610df8b74cdb462c211d889b403dcc6
Hunter Smith <hjsmit14@asu.edu>
2024-11-21 20:46:36 -0700

    as

--- src/listeners/block_action/help_view.js
+++ src/listeners/block_action/help_view.js
@@ -1,9 +1,8 @@
-import { helpModal, updateModalWithImages } from "../../user-interface/modules/help_modal_ui.js";
+import { helpModal } from "../../user-interface/modules/help_modal_ui.js";
 
 export async function helpview(slackActivity, web) {
     try {
         let modal = helpModal();
-        updateModalWithImages(modal);
         await web.views.open({
             trigger_id: slackActivity.trigger_id,
             view: modal
